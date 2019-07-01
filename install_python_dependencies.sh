181d5c09ed0a1ec048da1a7a8f84ff0229ebf492
pandreu <philippe.andreu@epfl.ch>
2019-07-01 20:10:58 +0200

    Progress on README, missing runtimes and some completion, updated install_python_dependencies to reflect current requirements

--- install_python_dependencies.sh
+++ install_python_dependencies.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
-pip install pathlib
+pip install argparse
+pip install matplotlib
 pip install numpy
 pip install pandas
-pip install matplotlib
-pip install seaborn
+pip install pathlib
+pip install patool
+pip install Pillow
+pip install PyWavelets
 pip install scikit-image
+pip install scikit-learn
+pip install seaborn
+pip install tensorflow
 pip install tqdm
+pip install xgboost
