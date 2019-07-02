Correcting DCGAN LS and adding optional rotation augmentation to data

DCGAN.py
@@ -83,7 +83,7 @@ class DCGAN:
                 added_perturbation = tf.concat([perturbation, -perturbation], axis=1)
 
                 smoothed_fakes = tf.one_hot(fake_labels, depth=2)
-                smoothed_reals = tf.one_hot(real_labels, depth=2) + added_perturbation
+                smoothed_reals = tf.clip_by_value(tf.one_hot(real_labels, depth=2) + added_perturbation, 0.0, 1.0)
                 
                 fake_loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=smoothed_fakes, logits=fake_out)
                 real_loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=smoothed_reals, logits=real_out)

data.py
@@ -7,12 +7,14 @@ from PIL import Image
 import layers
 
 global_seed=5 # for reproducibilty
+ROTATIONS = False
 
 def preprocess_image(image):
     image = tf.image.decode_png(image, channels=1) # grayscale images => 1 channel
     image = tf.cast(image,tf.float32) / 128. - 1
-    rotation = tf.random.uniform([1], minval=0, maxval=3, dtype=tf.dtypes.int32)
-    image = tf.image.rot90(image, k=rotation[0])
+    if(ROTATIONS):
+        rotation = tf.random.uniform([1], minval=0, maxval=3, dtype=tf.dtypes.int32)
+        image = tf.image.rot90(image, k=rotation[0])
     image = tf.transpose(a=image, perm=[2, 0, 1]) # images are read in channels_last format, so convert to channels_first format for performance on GPU
     return image
 