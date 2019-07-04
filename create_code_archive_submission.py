1a96a79d232278116d7af51166f5a6859dbd0406
ubuntu <mamrani@student.ethz.ch>
2019-07-04 09:35:11 +0200

    script to easily compress code to submit

---
+++ create_code_archive_submission.py
@@ -0,0 +1,44 @@
+import os, sys
+import patoolib
+
+
+out_file = "code+Readme.zip"
+if os.path.exists(out_file):
+    os.remove(out_file)
+
+submission_files = ["data.py",
+                    "layers.py",
+                    "utils.py",
+                    # baselines
+                    "generate_feats.py",
+                    "baseline_patches.py",
+                    "baseline_score_regressor.py",
+                    "baseline_score_regressor_test.py",
+                    # DCGAN
+                    "DCGAN.py",
+                    "train_DCGAN.py",
+                    "DCGAN_Scorer.py",
+                    "train_DCGAN_for_score.py",
+                    "test_DCGAN_scorer.py",
+                    # SRM
+                    "StackedSRM.py",
+                    "train_stackedSRM.py",
+                    # MCGAN
+                    "MCGAN.py",
+                    "train_MCGAN.py",
+                    # FullresGan
+                    "FullresGAN.py",
+                    "train_FullresGAN.py",
+                    # Generation Pipeline
+                    "test_GAN_SRM_Scorer.py",
+                    # Experiments
+                    "extract_features.py",
+                    "run_experiments.py",
+                    # Others
+                    "README.md",
+                    "requirements.txt"
+                   ]
+
+patoolib.create_archive(out_file, submission_files)
+    
+
