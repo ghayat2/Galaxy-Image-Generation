0bf5aa47f9b709bedd971596474cd64cec8d1366
ubuntu <mamrani@student.ethz.ch>
2019-07-05 08:48:56 +0200

    changes to downsample.py, works on tf 1.13

--- downsample.py
+++ downsample.py
@@ -1,31 +1,45 @@
 import utils
-import pathlib, os
+import pathlib, os, sys
 import numpy as np
 from tqdm import tqdm
-from skimage import color, io
+from PIL import Image
 from argparse import ArgumentParser
 import tensorflow as tf
 
 parser = ArgumentParser()
-parser.add_argument('-d', '--image_dir', type=str, default="./generated_images/")
-parser.add_argument('-o', '--out_dir', type=str, default="./downsampled")
-parser.add_argument('-v', '--vmin', type=int, default="0 if imgs in [0,1] range, -1 if images in [-1,1]")
 
+parser.add_argument('-d', '--images_dir', type=str, required=True, help="the directory containing images to downsample")
+parser.add_argument('-o', '--out_dir', type=str, default="./downsampled")
 
 args = parser.parse_args()
 
-resizer = utils.make_max_pooling_resizer(args.vmin)
+IMAGES_DIR = args.images_dir
+IMAGE_DIR_NAME = (IMAGES_DIR.rstrip("/")).split("/")[-1]
+OUT_DIR = os.path.join(args.out_dir, IMAGE_DIR_NAME)
 
-if not os.path.isdir(args.out_dir):
-    os.mkdir(args.out_dir)
+print("\n")
+print("Run infos:")
+print("    IMAGES_DIR: {}".format(IMAGES_DIR))
+print("    OUT_DIR: {}".format(OUT_DIR))
+print("\n")
+sys.stdout.flush()
 
-all_images = [str(item) for item in pathlib.Path(args.image_dir).glob('*')]
+#sys.exit(0)
+
+resizer = utils.make_max_pooling_resizer(vmin=0) # vmin=0 since by default pixel values read are in the range [0, 255]
+
+if not os.path.isdir(OUT_DIR):
+    os.makedirs(OUT_DIR)
+
+all_images = [str(item) for item in pathlib.Path(IMAGES_DIR).glob('*')]
+
 id = 0
 for image in tqdm(all_images):
-    decoded_image = color.rgb2gray(io.imread(image))
-    resized_image = resizer(np.expand_dims(np.expand_dims(decoded_image, axis=0), axis=-1))
-    resized_image = np.squeeze(resized_image)
-    io.imsave(os.path.join(args.out_dir, "image_" + str(id) + ".png"), resized_image)
+    decoded_image = np.array(Image.open(image))
+    resized_image = resizer.predict(np.expand_dims(np.expand_dims(decoded_image, axis=0), axis=-1))
+    resized_image = np.squeeze(resized_image).astype("uint8")
+    resized_image = Image.fromarray(resized_image)
+    resized_image.save(os.path.join(OUT_DIR, "image_" + str(id) + ".png"))
     id += 1
 
 print("{} images have been resized and saved in {}".format(id, args.out_dir))
